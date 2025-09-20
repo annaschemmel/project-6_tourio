f464b3744418caf486119dd3f4cf2cbec217e2b0
Anna Schemmel <anna.schemmel@gmx.de>
2025-09-20 13:46:42 +0200

    Add: DELETE

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -6,26 +6,43 @@ export default async function handler(request, response) {
   await dbConnect();
   const { id } = request.query;
 
-  if (request.method === "GET") {
-    const place = await Place.findById(id);
+  try {
+    if (request.method === "GET") {
+      const place = await Place.findById(id);
 
-    if (!place) {
-      response.status(404).json({ status: "Not found" });
-      return;
-    }
-    response.status(200).json(place);
-  }
-
-  if (request.method === "PUT") {
-    const placeData = request.body;
-    const place = await Place.findByIdAndUpdate(id, placeData);
-
-    if (!place) {
-      response.status(404).json({ status: "Not found" });
-      return;
+      if (!place) {
+        response.status(404).json({ status: "Not found" });
+        return;
+      }
+      response.status(200).json(place);
     }
 
-    response.status(200).json({ status: "place was updated" });
+    if (request.method === "PUT") {
+      const placeData = request.body;
+      const place = await Place.findByIdAndUpdate(id, placeData);
+
+      if (!place) {
+        response.status(404).json({ status: "Not found" });
+        return;
+      }
+
+      response.status(200).json({ status: "Place was updated" });
+    }
+
+    if (request.method === "DELETE") {
+      const place = await Place.findByIdAndDelete(id);
+      console.log("place:", place);
+
+      if (!place) {
+        response.status(404).json({ status: "Not found" });
+        return;
+      }
+
+      response.status(200).json({ status: "Place was deleted" });
+    }
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ message: "Internal Server Error" });
   }
 
   response.status(405).json({ status: "Method not allowed" });
